d29b84afa0100c3d83f9706da4b8164f89ba343a
Denver Gomes <denver.gomes@syspree.com>
2019-03-07 15:20:11 +0530

    07-03-2019 15:20 final commit

---
+++ build/Assets/js/tab.js
@@ -0,0 +1,20 @@
+function openCategory(evt, categoryName){
+    var i, tabcontent, tablinks;
+    tabcontent=document.getElementsByClassName("tabcontent");
+    
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    tablinks = document.getElementsByClassName("tablinks");
+
+
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+
+    // Show the current tab, and add an "active" class to the link that opened the tab
+    document.getElementById(categoryName).style.display = "block";
+    evt.currentTarget.className += " active";
+
+}
