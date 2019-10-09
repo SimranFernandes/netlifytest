3b7b78b00ffcf8eb4774a5227f1e82e3342341cf
Sanjay <praveen.poojari@syspree.com>
2019-10-09 17:40:09 +0530

    09-10-2019 05:39

--- src/Components/indexPage/body.jsx
+++ src/Components/indexPage/body.jsx
@@ -118,7 +118,7 @@ class Body extends Component {
         <div id="modal1" className="modal">
           <div className="modal-content">
             <iframe
-              src="http://uckindiesindia.in/contact/Feedback-Form/"
+              src="https://uckindiesindia.in/contact/Feedback-Form/"
               height="520"
               style={this.styles}
             />
